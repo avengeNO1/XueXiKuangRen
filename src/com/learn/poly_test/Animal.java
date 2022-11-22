c953aad41278fa992d7d990786040ab104b51d63
HuAng <sys_snow@163.com>
2022-11-22 11:45:41 +0800

    Java学习 数组、循环、排序、递归

---
+++ src/com/learn/poly_test/Animal.java
@@ -0,0 +1,35 @@
+package com.learn.poly_test;
+
+/**
+ * @author HuAng
+ */
+public class Animal {
+    public String name;
+    protected  int age;
+    String color;
+    private String address;
+
+    public void eat(){
+        System.out.println("吃饭了");
+    }
+    public void run(){
+        System.out.println("跑");
+    }
+    public void sleep(){
+        System.out.println("睡觉");
+    }
+    public void show(){
+        System.out.println("你好");
+    }
+//    public Animal() {
+//    }
+//
+//    public Animal(String name, int age, String color, String address) {
+//        this.name = name;
+//        this.age = age;
+//        this.color = color;
+//        this.address = address;
+//    }
+}
+
+   
---
+++ src/com/learn/poly_test/Cat.java
@@ -0,0 +1,23 @@
+package com.learn.poly_test;
+
+/**
+ * @author HuAng
+ */
+public class Cat extends Animal {
+    public void eat(){
+        System.out.println("猫吃鱼");
+    }
+    public void catchMouse(){
+        System.out.println("猫抓老鼠");
+    }
+//    public Cat() {
+//         super();
+//    }
+//
+//    public Cat(String name, int age, String color, String address) {
+//        super(name, age, color, address);
+//    }
+}
+
+
+   
